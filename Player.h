a2c874ec703b7835deb8b7f28e759cd4b4cffe0a
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-05-12 17:04:13 +0900

    修正

---
+++ Player.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include "Input.h"
+
+class Player 
+{
+public:
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(ViewProjection viewProjection);
+
+private:
+	WorldTransform worldTransform_;
+
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+
+	Input* input_ = nullptr;
+};
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,75 @@
 #include "GameScene.h"
 #include "TextureManager.h"
+#include "AxisIndicator.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() 
+{
+	delete model_;
+	delete player_;
+	delete debugCamera_;
+}
 
-void GameScene::Initialize() {
+void GameScene::Initialize() 
+{
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	textureHandle_ = TextureManager::Load("sample.png");
+	model_ = Model::Create();
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	// 自キャラの生成
+	player_ = new Player();
+	// 自キャラの初期化
+	player_->Initialize(model_, textureHandle_);
+
+	// デバッグカメラの生成
+	debugCamera_ = new DebugCamera(50, 50);
+
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	// 軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&viewProjection_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{
+	// 自キャラの更新
+	player_->Update();
+
+	debugCamera_->Update();
+
+	#ifdef _DEBUG
+
+	if (input_->TriggerKey(DIK_SPACE))
+	{
+		isDebugCameraActive_ = true;
+	}
+
+    #endif 
+
+	// カメラの処理
+	if (isDebugCameraActive_)
+	{
+		// デバッグカメラの更新
+		debugCamera_->Update();
+
+		viewProjection_.matView = debugCamera_->GetViewProjection().matView;
+		viewProjection_.matProjection = debugCamera_->GetViewProjection().matProjection;
+		// ビュープロジェクション行列の転送
+		viewProjection_.TransferMatrix();
+	} else {
+		// ビュープロジェクション行列の更新と転送
+		viewProjection_.UpdateMatrix();
+	}
+}
 
 void GameScene::Draw() {
 
@@ -41,6 +97,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	player_->Draw(viewProjection_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();

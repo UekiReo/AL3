e0708fc3c73421a5fbbe5b43f2e9b30abecab8f4
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-06-16 16:19:39 +0900

    TimeCallクラスの追加

---
+++ TimeCall.cpp
@@ -0,0 +1,25 @@
+﻿#include "TimeCall.h"
+
+TimeCall::TimeCall(std::function<void()> callBack, uint32_t time) 
+{
+	this->callBack_ = callBack;
+	this->time_ = time;
+}
+
+void TimeCall::Update() 
+{
+	if (isFinish) 
+	{
+		return;
+	}
+
+	time_--;
+
+	if (time_ <= 0) 
+	{
+		isFinish = true;
+
+		// コールバック関数の呼び出し
+		callBack_();
+	}
+}
---
+++ TimeCall.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <functional>
+
+/// <summary>
+/// 時限発動
+/// </summary>
+
+class TimeCall
+{
+public:
+	// コンストラクタ
+	TimeCall(std::function<void()> callBack, uint32_t time);
+
+	// 更新
+	void Update();
+
+	// 完了ならtrueを返す
+	bool IsFinished() { return isFinish; }
+
+private:
+	// コールバック
+	std::function<void()> callBack_;
+
+	// 残り時間
+	uint32_t time_;
+
+	// 完了フラグ
+	bool isFinish = false;
+};

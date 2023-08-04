8af5b406eb94aee614a32c40430869387d7b10a5
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-08-04 12:01:55 +0900

    imguiの修正

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -18,7 +18,7 @@ void RailCamera::Update()
 
 	viewProjection_.matView = Inverse(worldTransform_.matWorld_);
 
-	ImGui::Begin("Camera");
+	ImGui::Begin("RailCamera");
 	ImGui::DragFloat3("CameraTranslation", &worldTransform_.translation_.x, 0.01f);
 	ImGui::DragFloat3("CameraRotate", &worldTransform_.rotation_.x, 0.01f);
 	ImGui::End();
